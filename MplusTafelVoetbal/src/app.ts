2d8baac0225597470afc612465d2f46374ad9626
Marc Willemsen <marc@bolt.nl>
2020-05-16 17:24:32 +0200

    classes lesson after extended

---
+++ MplusTafelVoetbal/src/app.ts
@@ -0,0 +1,9 @@
+const speler1= document.getElementById('Speler 1');
+const speler2= document.getElementById('Speler 1');
+const form = document.getElementById('form');
+
+form?.addEventListener('submit', (e) => {
+
+   console.log(speler1)
+
+})
--- main/src/app.ts
+++ main/src/app.ts
@@ -1,20 +1,83 @@
 class Department {
-    name: string;
+  // private id: string;
+  // public name: string;
+  protected employees: string[] = [];
 
-    constructor(n:string){
-        this.name = n;
-    }
-    
-    describe(){
-        console.log('Department: ' +this.name)
-    }
+  constructor(private readonly id: string, public name: string) {
+    // this.id= id;
+    // this.name = n;
+  }
 
+  describe(this: Department) {
+    console.log(`Department (${this.id}):  ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    // this.id = "d2";
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
-const accounting = new Department('Accounting');
-const sales = new Department('Sales');
+class ITDepartment extends Department{
+   admins: string[];
+    constructor(id: string, admins: string[]){
+        super(id, 'Accounting');
+        this.admins = admins;
+    }
+}
 
-// console.log(accounting);
+class AccountingDepartment extends Department {
+    constructor(id: string, private reports: string[]){
+        super(id, 'IT');
+        
+    }
+    addEmployee(name:string){
+        if(name=== "Max") {
+            console.log("You cant use the name max! ");
+            return ;
+        }
+        this.employees.push(name);
+    }
 
-accounting.describe();
-sales.describe();
+    addReport(text: string){
+        this.reports.push(text);
+    }
+    printReports(){
+        console.log(this.reports);
+    }
+}
+
+
+const itDepartment = new ITDepartment("d2", ["max"]);
+// const accounting = new Department("d1", "Accounting");
+
+// accounting.addEmployee("max");
+// accounting.addEmployee("Manu");
+// accounting.addEmployee("anna");
+
+itDepartment.addEmployee("max");
+itDepartment.addEmployee("Manu");
+itDepartment.addEmployee("anna");
+
+// accounting.employees[2] = "anna";
+
+// accounting.describe();
+// accounting.printEmployeeInformation();
+
+itDepartment.describe();
+itDepartment.printEmployeeInformation();
+
+console.log(itDepartment);
+
+const accounting = new AccountingDepartment("a1",[]);
+accounting.printReports();
+accounting.addReport("somehting went wrong...");
+accounting.printReports();
+accounting.addEmployee("Max");
+accounting.addEmployee("Marc");
+accounting.printEmployeeInformation();
