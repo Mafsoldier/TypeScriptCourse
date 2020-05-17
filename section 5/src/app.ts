d7f75882d36c10f783eb660c3494845478518379
Marc Willemsen <marc@bolt.nl>
2020-05-17 21:16:40 +0200

    start section 6

---
+++ section 5/src/app.ts
@@ -0,0 +1,62 @@
+// an interface describes the structure of an object
+
+
+// type AddFn = (a: number, b: number) => number;
+
+interface AddFn {
+    (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+    return n1 + n2;
+};
+
+interface Named {
+    readonly name?: string;
+    outputName?: string;
+}
+
+
+interface Greetable extends Named {
+    greet(phrase: string): void;
+}
+
+class Person implements Greetable, Named {
+    name?: string;
+    outputName = '... ';
+    age = 30;
+
+    constructor(n?: string) {
+        if(n){
+            this.name = n;
+        }
+        
+    }
+
+    greet(phrase: string) {
+        if(this.name){
+            console.log(phrase + ' ' + this.name)
+        }else {
+            console.log("Hi..");
+        }
+        
+    }
+
+
+}
+
+let user1: Greetable;
+let user2: Greetable;
+
+user1 = new Person("marc");
+user2 = new Person();
+
+// user1.name = "marjanne";
+
+user1.greet("hi there - I am ");
+user2.greet("hello there.. ");
+
+console.log(user1);
+
