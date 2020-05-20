a822e1c6b07bc281c0c8c50c2df8054fde2e2fce
Marc Willemsen <marc@bolt.nl>
2020-05-20 15:57:59 +0200

    s

--- main/src/app.ts
+++ main/src/app.ts
@@ -31,7 +31,7 @@ function WithTemplate(template: string, hookID: string){
 @Logger2('Logging - Person') 
 @WithTemplate('<h1>My Person Object</h1>', 'app') //this will be first 
 class Person {
-    name = 'Max';
+    name = 'Marcs';
 
     constructor() {
         console.log('Creating person object... ');
@@ -41,3 +41,33 @@ class Person {
 const pers = new Person();
 console.log(pers);
 
+//---
+
+function Log(target: any, ) {
+
+}
+
+
+class Product {
+    @log
+    title: string;
+    private _price: number;
+
+    set price(val: number){
+        if (val >0){
+            this._price = val;
+        } else {
+            throw new Error('Invalid price');
+        }
+    }
+
+    constructor (t: string, p :number) {
+        this.title = t;
+        this._price = p;
+    }
+
+    getPriceWithTax (tax:number) {
+        return this._price * (1 + tax);
+
+    }
+}
