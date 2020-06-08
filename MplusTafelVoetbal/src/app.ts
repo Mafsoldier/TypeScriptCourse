fcf1e9c6150e65bb961c3ed1ea4242f9f9294399
Marc Willemsen <marc@bolt.nl>
2020-06-08 17:00:15 +0200

    tafelvoetbalprojectje

--- MplusTafelVoetbal/src/app.ts
+++
@@ -1,9 +0,0 @@
-const speler1= document.getElementById('Speler 1');
-const speler2= document.getElementById('Speler 1');
-const form = document.getElementById('form');
-
-form?.addEventListener('submit', (e) => {
-
-   console.log(speler1)
-
-})
